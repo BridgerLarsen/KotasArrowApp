c74aaaa20f0c72587ea9a8755b3b06771efa3d0b
Bridger Larsen <bridgerjimmy@gmail.com>
2021-07-21 19:24:00 -0600

    Edited server js file where we serving static file of index.html in our environment check

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use('/api/users', users)
 app.use('/api/auth', auth);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
